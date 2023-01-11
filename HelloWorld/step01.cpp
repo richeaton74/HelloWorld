577954a6f56240c13ad66d1366fb7d0f9a32c0cd
Rich Eaton <richieeaton@yahoo.co.uk>
2023-01-11 07:17:09 +0000

    Added .cpp and .hpp files Swift/C++

---
+++ HelloWorld/step01.cpp
@@ -0,0 +1,16 @@
+//
+//  step01.cpp
+//  HelloWorld
+//
+//  Created by Richard Eaton on 11/01/2023.
+//
+
+#include "step01.hpp"
+#include <iostream>
+
+int main()
+
+{
+    std::cout << "Hello World!" << std::endl;
+    std::cin.get();
+}
